ae8f6b797e16d98a2fb95a9b09339a4de85f38e5
Jai Kishan <jaikishanv98@gmail.com>
2019-07-14 20:30:14 +0530

    Initial commit with ttlhashmap, datastore with CRD operaions

---
+++ DataStore.java
@@ -0,0 +1,128 @@
+import java.util.*;
+import java.io.*;
+import java.nio.file.*;
+import java.nio.charset.*;
+import java.util.concurrent.TimeUnit;
+
+public class DataStore {
+    public TtlHashMap<String, Path> myMap = new TtlHashMap<String, Path>();
+    
+    String defaultPath = "D:\\";
+    private static String getContent(String filePath)
+    {
+        StringBuilder contentBuilder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath)))
+        {
+
+            String sCurrentLine;
+            while ((sCurrentLine = br.readLine()) != null)
+            {
+                contentBuilder.append(sCurrentLine).append("\n");
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return contentBuilder.toString();
+    }
+
+    public void create() throws Exception{
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("enter key in caps");
+    	String key = sc.next();
+    	if(!key.equals(key.toUpperCase())) {
+    		System.out.println("key should have upper case letters only");
+    		return;
+    	}
+    	if(key.length()>32){
+    		System.out.println("key should have less than 32 chars");
+    		return;
+    	}
+    	if(myMap.containsKey(key)){
+    		System.out.println("key already exists");
+    	}
+
+    	System.out.println("Do you have file location?(y/N):");
+    	String ch = sc.next();
+    	if (ch.charAt(0)=='y') {
+    		System.out.print("Enter path: ");
+	   		String path = sc.next();
+	   		Path osPath = Paths.get(path);
+		   	if (!Files.exists(osPath)) {
+		   		System.out.println("INVALID path");
+	   			return;
+	   		}
+		   	if(!path.endsWith(".txt")){
+		   		System.out.println("Not text file");
+	   			return;
+	   		}
+	   		System.out.println("enter time to live in seconds, enter 0 if no ttl");
+	   		long ttl = sc.nextLong();
+	   		myMap.put(key, osPath);
+	   		myMap.putTtl(key, TimeUnit.SECONDS, ttl);
+    	}
+    	else if(ch.charAt(0)=='N'){
+    		List<String> lines = Arrays.asList(key);
+			Path file = Paths.get(defaultPath+key+".txt");
+			Files.write(file, lines, StandardCharsets.UTF_8);
+			System.out.println("enter time to live in seconds, enter 0 if no ttl");
+	   		long ttl = sc.nextLong();
+	   		myMap.put(key, file);
+	   		myMap.putTtl(key, TimeUnit.SECONDS, ttl);
+    	}
+    	else{System.out.println("invalid Character");}
+
+
+    }
+
+    private void read() {
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("Enter key:");
+    	String key = sc.next();
+    	if(myMap.containsKey(key)){
+    		Path path = myMap.get(key);
+    		String content = getContent(path.toString());
+    		System.out.println(content);
+    	}
+    	else{
+    		System.out.println("INVALID key");
+    	}
+    }
+
+    private void delete() {
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("Enter key:");
+    	String key = sc.next();
+    	if(myMap.containsKey(key)){
+    		Path path = myMap.remove(key);
+    		System.out.println("Deleted the "+path.toString()+" file from store!!");
+    	}
+    	else{
+    		System.out.println("INVALID key");
+    	}
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+    	DataStore ds = new DataStore();
+    	try{
+    		while(true){
+    			System.out.print("1.create 2.read 3.delete 4.exit\nenter choice:");
+    			int choice = sc.nextInt();
+    			switch(choice){
+    				case 1: ds.create();break;
+    				case 2: ds.read();
+    						break;
+    				case 3: ds.delete();
+    						break;
+    				case 4: return;
+    				default: System.out.println("invalid choice!!");
+    			}
+    	}
+    	}catch(Exception e){
+    		System.out.println(e.toString());
+    		e.printStackTrace();
+    	}
+    }
+}
---
+++ TtlHashMap.java
@@ -0,0 +1,115 @@
+import static java.util.Collections.*;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class TtlHashMap<K, V> implements Map<K, V> {
+
+    private final HashMap<K, V> store = new HashMap<>();
+    private final HashMap<K, Long> timestamps = new HashMap<>();
+    private final HashMap<K, Long> ttl = new HashMap<>();
+
+    public void putTtl(K key, TimeUnit ttlUnit, long ttlValue) {
+        ttl.put(key, ttlUnit.toNanos(ttlValue));
+        clearExpired();
+    }
+
+    @Override
+    public V get(Object key) {
+        V value = this.store.get(key);
+
+        if (value != null && expired(key, value)) {
+            ttl.remove(key);
+            store.remove(key);
+            timestamps.remove(key);
+            return null;
+        } else {
+            return value;
+        }
+    }
+
+    private boolean expired(Object key, V value) {
+        if(ttl.get(key)==0){
+            return false;
+        }
+        return (System.nanoTime() - timestamps.get(key)) > ttl.get(key);
+    }
+
+    @Override
+    public V put(K key, V value) {
+        clearExpired();
+        timestamps.put(key, System.nanoTime());
+        return store.put(key, value);
+    }
+
+    @Override
+    public int size() {
+        return store.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return store.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        clearExpired();
+        return store.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        clearExpired();
+        return store.containsValue(value);
+    }
+
+    @Override
+    public V remove(Object key) {
+        timestamps.remove(key);
+        ttl.remove(key);
+        return store.remove(key);
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+        for (Map.Entry<? extends K, ? extends V> e : m.entrySet()) {
+            this.put(e.getKey(), e.getValue());
+        }
+    }
+
+    @Override
+    public void clear() {
+        ttl.clear();
+        timestamps.clear();
+        store.clear();
+    }
+
+    @Override
+    public Set<K> keySet() {
+        clearExpired();
+        return unmodifiableSet(store.keySet());
+    }
+
+    @Override
+    public Collection<V> values() {
+        clearExpired();
+        return unmodifiableCollection(store.values());
+    }
+
+    @Override
+    public Set<java.util.Map.Entry<K, V>> entrySet() {
+        clearExpired();
+        return unmodifiableSet(store.entrySet());
+    }
+
+    private void clearExpired() {
+        for (K k : store.keySet()) {
+            this.get(k);
+        }
+    }
+
+}
